a6e9cab14922fab7296065edc6558a93542b44ab
Israel Alves <israelalvesmelo@hotmail.com>
2024-09-10 21:44:01 -0300

    learn CLI with cobra-cli

---
+++ CLI/cmd/category.go
@@ -0,0 +1,23 @@
+/*
+Copyright © 2024 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"github.com/spf13/cobra"
+)
+
+// categoryCmd represents the category command
+var categoryCmd = &cobra.Command{
+	Use:   "category",
+	Short: "A brief description of your command",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		cmd.Help()
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(categoryCmd)
+}
