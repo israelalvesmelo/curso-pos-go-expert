57b981fb10d1de2cbc9688ed00d1c1e00a63e4bf
Israel Alves <israelalvesmelo@hotmail.com>
2024-11-01 21:08:19 -0300

    added list orders

--- CleanArch/internal/infra/database/order_repository.go
+++ CleanArch/internal/infra/database/order_repository.go
@@ -26,12 +26,20 @@ func (r *OrderRepository) Save(order *entity.Order) error {
 	return nil
 }
 
-// TODO: Validar se isso é necessário
-func (r *OrderRepository) GetTotal() (int, error) {
-	var total int
-	err := r.Db.QueryRow("Select count(*) from orders").Scan(&total)
+func (r *OrderRepository) FindAll() ([]entity.Order, error) {
+	rows, err := r.Db.Query("SELECT id, price, tax, final_price FROM orders")
 	if err != nil {
-		return 0, err
+		return nil, err
 	}
-	return total, nil
+	defer rows.Close()
+	orders := []entity.Order{}
+	for rows.Next() {
+		var id string
+		var price, tax, finalPrice float64
+		if err := rows.Scan(&id, &price, &tax, &finalPrice); err != nil {
+			return nil, err
+		}
+		orders = append(orders, entity.Order{ID: id, Price: price, Tax: tax, FinalPrice: finalPrice})
+	}
+	return orders, nil
 }
